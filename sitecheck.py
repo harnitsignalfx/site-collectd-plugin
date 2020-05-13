ebce706cc96b6a4612a3586de08bac3a3d1d4f4a
Harnit Singh <harnit.singh@signalfx.com>
2020-05-12 23:06:20 -0700

    fixed indentation and used proper decoding for <html> files

--- sitecheck.py
+++ sitecheck.py
@@ -14,25 +14,25 @@ TIMEOUT = 10
 def config_callback(conf):
 
 	for node in conf.children:
-        	try:
-            		if node.key == 'Regex':
-            			global REGEX
-                		REGEX = node.values[0]
-            		elif node.key == 'Interval':
-                		global INTERVAL
-                		INTERVAL = node.values[0]
-            		elif node.key == 'URL':
-             		        global URL
-                		URL = node.values[0]
-            		elif node.key == 'Name':
-                		global METRIC_NAME
-                		METRIC_NAME = node.values[0]
-            		elif node.key == 'Timeout':
-                		global TIMEOUT
-                		TIMEOUT = node.values[0]
-        	except Exception as e:
-            		collectd.error('Failed to load the configuration %s due to %s' % (node.key, e))
-            		raise e
+			try:
+				if node.key == 'Regex':
+					global REGEX
+					REGEX = node.values[0]
+				elif node.key == 'Interval':
+					global INTERVAL
+					INTERVAL = node.values[0]
+				elif node.key == 'URL':
+					global URL
+					URL = node.values[0]
+				elif node.key == 'Name':
+					global METRIC_NAME
+					METRIC_NAME = node.values[0]
+				elif node.key == 'Timeout':
+					global TIMEOUT
+					TIMEOUT = node.values[0]
+			except Exception as e:
+					collectd.error('Failed to load the configuration %s due to %s' % (node.key, e))
+					raise e
 
 def init_callback():
 	global pattern
@@ -52,7 +52,7 @@ def read_callback():
 
 		m = 0
 		if response.status_code == 200:
-			m = pattern.findall(response.content)
+			m = pattern.findall(response.content.decode('utf-8'))
 		#collectd.info('Matches - %s' % (m))
 
 		# Metric Names to be sent (# of Matches, Status Code, Server Response Time, Client Response Time)
@@ -70,11 +70,15 @@ def read_callback():
 		collectd.error('Failed to fetch and transfer data due to %s' % (e))
 
 def dispatch_values(type_instance,values):
+	#dims = {}
+	#dims['dim1']='value1'
+	#dims['dim2']='value2'
+	#val.plugin_instance = 'site-plugin-instance'
+	#val.plugin_instance += '[{dims}]'.format(dims=','.join(['='.join(d) for d in dims.items()]))
 	val = collectd.Values(type='gauge')
 	val.type_instance = type_instance
 	val.plugin = 'site-regex-plugin'
 	val.values = [values]
-	#collectd.info('type_instance %s , values -> %s , val -> %s' %(type_instance,values,val))
 	val.dispatch()
 
 collectd.register_config(config_callback)
